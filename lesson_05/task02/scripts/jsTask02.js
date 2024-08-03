ab2c9f7b7d0aba4dc82335e91948e6abe34e1496
DaniloFreelancer <144622931+DaniloFreelancer@users.noreply.github.com>
2024-08-03 18:30:41 +0300

    Delete lesson_05 directory

--- lesson_05/task02/scripts/jsTask02.js
+++
@@ -1,6 +0,0 @@
-for (let monthNumber = 3; monthNumber <= 8; monthNumber++) {
-  if (monthNumber < 6) 
-    alert(`Номер місяця весни ${monthNumber}`);
-  else
-  	alert(`Номер місяця літа ${monthNumber}`)
-}
