2df6b77e92e77379731a818f98781caad6c88c58
DaniloFreelancer <144622931+DaniloFreelancer@users.noreply.github.com>
2024-09-29 21:33:36 +0300

    Add files via upload

--- lesson_18/task03/scripts/jsTask03.js
+++ lesson_18/task03/scripts/jsTask03.js
@@ -4,7 +4,6 @@ if (confirm(`Почати тестування ?`)) {
   window.onload = function () {
     document.getElementById("calculate-age").onclick = function () {
       const birthYear = parseInt(document.getElementById("birthYear").value);
-		if(birthYear > 2024 || isNaN) throw new Error(`Введено некоректні дані!`)
       const currentYear = new Date().getFullYear();
       const age = currentYear - birthYear;
 
