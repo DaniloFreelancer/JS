995a90e00ebca0acead28190441ca21bd702139c
DaniloFreelancer <144622931+DaniloFreelancer@users.noreply.github.com>
2024-09-29 21:55:09 +0300

    Add files via upload

--- lesson_18/task03/scripts/jsTask03.js
+++ lesson_18/task03/scripts/jsTask03.js
@@ -1,13 +1,24 @@
 "use strict";
 
 if (confirm(`Почати тестування ?`)) {
-  window.onload = function () {
-    document.getElementById("calculate-age").onclick = function () {
-      const birthYear = parseInt(document.getElementById("birthYear").value);
-      const currentYear = new Date().getFullYear();
+  function userAge() {
+    const birthYear = parseInt(document.getElementById("birthYear").value);
+    const currentYear = new Date().getFullYear();
+    try {
+      if (
+        birthYear > new Date().getFullYear() ||
+        isNaN(birthYear) ||
+        birthYear <= 0
+      )
+        throw new Error(`Некоректне значення!`);
       const age = currentYear - birthYear;
 
       document.getElementById("age").value = age;
-    };
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+  window.onload = function () {
+    document.getElementById("calculate-age").onclick = userAge;
   };
 }
